10f058559015fe69315c4ae9d9fd5aefd81f8f08
Schmocker K. <kasparsimon.schmocker@students.bfh.ch>
2015-11-27 14:48:21 +0100

    medicament with name

--- src/main/java/ch/bfh/btx8081/w2015/red/Schlurp/mediplan/Medicament.java
+++ src/main/java/ch/bfh/btx8081/w2015/red/Schlurp/mediplan/Medicament.java
@@ -5,16 +5,35 @@ package ch.bfh.btx8081.w2015.red.Schlurp.mediplan;
 
 /**
  * @author Kaspar
+ * @version V27.11.2015
  *
  */
 public class Medicament {
 
 
+	private String name;
+
 	/**
+	 * @param name 
 	 * 
 	 * 
 	 */
-	public Medicament() {
+	public Medicament(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
