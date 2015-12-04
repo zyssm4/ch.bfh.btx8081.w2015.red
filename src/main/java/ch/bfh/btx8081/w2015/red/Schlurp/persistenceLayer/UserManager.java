596b132a0727a25761d28659cc4a63f6d11ec876
tschm2 <emautsc2@gmail.com>
2015-12-04 15:59:42 +0100

    UserManager Updated

--- src/main/java/ch/bfh/btx8081/w2015/red/Schlurp/persistenceLayer/UserManager.java
+++ src/main/java/ch/bfh/btx8081/w2015/red/Schlurp/persistenceLayer/UserManager.java
@@ -5,25 +5,31 @@ import ch.bfh.btx8081.w2015.red.Schlurp.infopage.Infopage;
 
 public class UserManager {
 
-	private static UserManager oc = null;
+	private static UserManager uc = null;
 	private Infopage infopage = null;
 	private String user = null;
 	private final String FILETYPE = ".txt";
+
 
 	private UserManager() {
 	}
 
 	public static UserManager getInstance() {
-		if (oc == null) {
-			oc = new UserManager();
+		if (uc == null) {
+			uc = new UserManager();
 		}
-		return oc;
+		return uc;
 	}
 
 	public void createInfoPageObject(String username) {
 		infopage = Infopage.readInformationsFromFile(username + FILETYPE);
 	}
-
+	
+	public void saveInfopage(Infopage iP){
+		infopage = iP;
+		Infopage.writeInfomationsToFile(infopage, user);
+	}
+	
 	public Infopage getInfopage() {
 		return infopage;
 	}
