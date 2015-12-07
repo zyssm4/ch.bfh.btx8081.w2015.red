e8b46cc310747f76efa8fb631717263437d840a8
Schmocker K. <kasparsimon.schmocker@students.bfh.ch>
2015-12-07 10:13:35 +0100

    added dose, interval, start and end date

--- src/main/java/ch/bfh/btx8081/w2015/red/Schlurp/mediplan/Medicament.java
+++ src/main/java/ch/bfh/btx8081/w2015/red/Schlurp/mediplan/Medicament.java
@@ -3,15 +3,22 @@
  */
 package ch.bfh.btx8081.w2015.red.Schlurp.mediplan;
 
+import java.util.Date;
+
 /**
  * @author Kaspar
- * @version V27.11.2015
+ * @version V07.12.2015
  *
  */
 public class Medicament {
 
 
 	private String name;
+	private int[] dose = new int[4];
+	private int interval;
+	private Date start;
+	private Date end;
+	
 
 	/**
 	 * @param name 
@@ -20,6 +27,27 @@ public class Medicament {
 	 */
 	public Medicament(String name) {
 		this.name = name;
+	}
+	/**
+	 * 
+	 * @param name
+	 * @param dose1
+	 * @param dose2
+	 * @param dose3
+	 * @param dose4
+	 * @param interval
+	 * @param start
+	 * @param end
+	 */
+	public Medicament(String name, int dose1, int dose2, int dose3, int dose4, int interval, Date start, Date end){
+		this.name = name;
+		dose[0] = dose1;
+		dose[1] = dose2;
+		dose[2] = dose3;
+		dose[3] = dose4;
+		this.interval = interval;
+		this.start = start;
+		this.end = end;
 	}
 
 	/**
@@ -35,5 +63,80 @@ public class Medicament {
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	/**
+	 * @return the dose
+	 */
+	public int[] getDose() {
+		return dose;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public int getDoseMorning(){
+		return dose[0];
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public int getDoseMidday(){
+		return dose[1];
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public int getDoseEvening(){
+		return dose[2];
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public int getDoseNight(){
+		return dose[3];
+	}
+	/**
+	 * @param dose the dose to set
+	 */
+	public void setDose(int[] dose) {
+		this.dose = dose;
+	}
+	/**
+	 * @return the interval
+	 */
+	public int getInterval() {
+		return interval;
+	}
+	/**
+	 * @param interval the interval to set
+	 */
+	public void setInterval(int interval) {
+		this.interval = interval;
+	}
+	/**
+	 * @return the start
+	 */
+	public Date getStart() {
+		return start;
+	}
+	/**
+	 * @param start the start to set
+	 */
+	public void setStart(Date start) {
+		this.start = start;
+	}
+	/**
+	 * @return the end
+	 */
+	public Date getEnd() {
+		return end;
+	}
+	/**
+	 * @param end the end to set
+	 */
+	public void setEnd(Date end) {
+		this.end = end;
+	}
 }
