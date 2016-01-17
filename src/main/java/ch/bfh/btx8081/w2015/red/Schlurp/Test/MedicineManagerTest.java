70c17ca515ac476755b66d2c4b77d2fe9c1e8984
zyssm4 <mauricepierre.zysset@students.bfh.ch>
2016-01-17 17:39:08 +0100

    JUnit Test MedicineManager

---
+++ src/main/java/ch/bfh/btx8081/w2015/red/Schlurp/Test/MedicineManagerTest.java
@@ -0,0 +1,52 @@
+package ch.bfh.btx8081.w2015.red.Schlurp.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import ch.bfh.btx8081.w2015.red.Schlurp.mediplan.Medicament;
+import ch.bfh.btx8081.w2015.red.Schlurp.persistenceLayer.MedicineManager;
+
+/**
+ * @author zyssm4
+ * @version V17.01.2016
+ * 
+ * Tests the Save and Remove Function of the MedicineManager
+ *
+ */
+public class MedicineManagerTest {
+	Date datestart = new Date();
+	Date dateend = new Date();
+	String name = "Aspirin";
+	Medicament testMediFull = new Medicament("Aspirin", "1", "6", datestart,
+			dateend);
+	MedicineManager mc = MedicineManager.getInstance();
+
+	/**
+	 * Create a new Medicament Aspirin with the Values from the earlier createt Medicament testMediFull.
+	 * @result The Medicament will be saved in the Medilist of the MedicinManager.
+	 */
+	@Test
+	public void testSaveMedicament() {
+		
+		mc.saveMedicament("Aspirin", testMediFull);
+		System.out.println(mc.getMediList().get(0));
+		assertSame(mc.getMediList().get(0), testMediFull);
+		
+
+	}
+	
+	/**
+	 * Remove the Medicament Aspirin from the MediList.
+	 * @result The Medicament will be removed in the Medilist of the MedicinManager.
+	 */
+	@Test
+	public void testRemoveMedicament() {
+		mc.removeMedicament(testMediFull);
+		assertTrue(mc.getMediList().isEmpty());
+	
+	}
+
+}
